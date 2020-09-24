32c7d5945512ab9f3cd3fac1fee122c6d7575c03
Dima Serebrennikov <seredim6@gmail.com>
2020-09-24 16:47:16 +0300

    add task_1

---
+++ TASK_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TASK_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привiт Донну!");
+        }
+    } 
+}
