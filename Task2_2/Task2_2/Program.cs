d08ba33b8924c7378c3c206dbcd977b8953b3c55
Dima Serebrennikov <seredim6@gmail.com>
2020-09-29 22:51:59 +0300

    add

---
+++ Task2_2/Task2_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            double b;
+            decimal c;
+            Console.WriteLine("Int:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Doulbe:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Long:");
+            c = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("a = {0} b = {1}, c = {2}", a, b, c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Task_3/Task_3/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            double b = 0;
+            decimal c = 0;
+            bool t = true;
+            
+            
+            
+            
+            
+            do
+            {
+                t = true;
+                try
+                {
+                    Console.WriteLine("Int:");
+                    a = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    t = false;
+                    Console.WriteLine("Incorrect format");
+                }
+            }
+            while (t == false);
+
+
+
+
+
+            do
+            {
+                t = true;
+                try
+                {
+                    Console.WriteLine("Doulbe:");
+                    b = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    t = false;
+                    Console.WriteLine("Incorrect format");
+                }
+            }
+            while (t == false);
+
+
+
+
+
+
+            do
+            {
+                t = true;
+                try
+                {
+                    Console.WriteLine("Long:");
+                    c = Convert.ToDecimal(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    t = false;
+                    Console.WriteLine("Incorrect format");
+                }
+            }
+            while (t == false);
+
+
+
+            Console.WriteLine("a = {0} b = {1}, c = {2}", a, b, c);
+        }
+    }
+}
